1addda110c66741b61b339671c1791340355145e
accelor-microsystems <rnd@accelorindia.com>
2024-06-04 18:01:44 +0530

    Update Table.jsx

--- src/app/_components/Table.jsx
+++ src/app/_components/Table.jsx
@@ -54,15 +54,15 @@ export default function Table({ data, setEditWindow, setEditData }) {
                                 <td className="text-center border-t p-2">{item.partNumber}</td>
                                 <td className="text-center border-t p-2">{item.partialPartNumber}</td>
                                 <td className="text-center border-t p-2">{item.description}</td>
-                                <td className="text-center border-t p-2">
-                                    {/* <form>
+{/*                                 <td className="text-center border-t p-2">
+                                    <form>
                                         <input name='id' type='hidden' value={item._id} />
                                         <button className='mx-4' onClick={(e) => handleEdit(e, item._id)}>
                                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.2} stroke="green" className="size-5">
                                                 <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                                             </svg>
                                         </button>
-                                    </form> */}
+                                    </form>
                                     <form>
                                         <input name='id' type='hidden' value={item._id} />
                                         <button onClick={() => handleDelete(item._id)}>
@@ -72,7 +72,7 @@ export default function Table({ data, setEditWindow, setEditData }) {
 
                                         </button>
                                     </form>
-                                </td>
+                                </td> */}
                             </tr>
                         )
                     })}
