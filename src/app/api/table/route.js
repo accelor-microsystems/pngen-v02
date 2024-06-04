7541b4a354124e1383e69da93fb6f0b6a6591b10
accelor-microsystems <rnd@accelorindia.com>
2024-06-04 18:48:15 +0530

    Update route.js

--- src/app/api/table/route.js
+++ src/app/api/table/route.js
@@ -1,12 +1,23 @@
 import { NextResponse } from "next/server";
-import connectMongoDB from "../../../lib/mongodb";
-import MPN from "../../../models/mpn";
+import connectMongoDB from "@/lib/mongodb";
+import MPN from "@/models/mpn";
 
 
-export async function GET(request) {
+export async function GET() {
     connectMongoDB();
-    const data = await MPN.find()
-    return NextResponse.json(data)
+    try {
+        const data = await MPN.find()
+        const response = NextResponse.json(data)
+        response.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        response.headers.set('Pragma', 'no-cache');
+        response.headers.set('Expires', '0');
+        response.headers.set('Surrogate-Control', 'no-store');
+
+        return response;
+    }
+    catch (err) {
+        return NextResponse.json({ messge: 'err' })
+    }
 
 }
 
