d1bfb1ad92f8206cfd79b0d85e81ba1aa7533acf
accelor-microsystems <rnd@accelorindia.com>
2024-06-13 10:17:49 +0530

    Delete src directory

--- src/app/_components/SpinningLoader.jsx
+++
@@ -1,8 +0,0 @@
-export default function SpinningLoader() {
-    return (
-        <div className="loader z-40">
-
-            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
-        </div>
-    )
-}
