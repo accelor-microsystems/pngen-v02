d7c9fc74bd0d465111116e2674ed5e1f212e54d0
accelor-microsystems <rnd@accelorindia.com>
2024-06-18 15:48:44 +0530

    Update route.js

--- src/app/api/production/fetchPartnumber/route.js
+++ src/app/api/production/fetchPartnumber/route.js
@@ -5,10 +5,11 @@ import ProductionPartnumber from "@/models/production/partnumber";
 export async function GET(req) {
     connectMongoDB();
     const { searchParams } = new URL(req.url)
+    const project = searchParams.get('project')
     const category = searchParams.get('category')
     console.log(category)
     try {
-        const data = await ProductionPartnumber.findOne({ category: category }).sort({ partialPartNumber: -1 })
+        const data = await ProductionPartnumber.findOne({ project: project, category: category }).sort({ partialPartNumber: -1 })
         return NextResponse.json(data)
 
     }
