1d5dec2cdeeaf739db94a7544538076ffb11f71a
accelor-microsystems <rnd@accelorindia.com>
2024-06-21 14:43:08 +0530

    Update page.jsx

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -56,15 +56,16 @@ export default function Login({ setLoggedIn }) {
 
     return (
         <div className=" flex-[2]  max-sm:flex-col h-[100vh]">
-            <div className="flex relative   items-center justify-center flex-col h-full gap-2 bg-gray-100">
-                <h1 className="text-[2.4rem] max-sm:text-[1.8rem] font-bold my-5">
-                    Log in to proceed
-                </h1>
-                <div className="flex flex-col w-[40%] max-sm:w-full px-4 gap-2">
+            <div className="flex relative     items-center justify-center flex-col h-full gap-2 bg-gray-100">
+                {/* <div className="absolute font-extrabold top-0 text-[10rem] text-gray-200 opensans">Welcome to PNGEN</div> */}
+                <div className="flex flex-col w-[40%] max-sm:w-full px-7 gap-2 bg-glass py-10">
+                    <h1 className="text-[2rem] mb-5  max-sm:text-[1.8rem] font-bold">
 
-                    <input className="border p-2 outline-none " value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Enter username" />
-                    <input className="border p-2 outline-none " value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter password" />
-                    <button className="bg-green-700 px-5 mt-3  py-2 text-white hover:bg-green-600" onClick={handleSignIn}>Sign in</button>
+                        Log in to proceed
+                    </h1>
+                    <input className="border-l-4 border-slate-300 p-3 outline-none shadow-sm focus:border-slate-500 " value={username} onChange={(e) => setUsername(e.target.value)} type="text" placeholder="Enter username" />
+                    <input className="border-l-4 border-slate-300 p-3 outline-none shadow-sm focus:border-slate-500 " value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter password" />
+                    <button className="bg-green-700 px-5 mt-3 btn-styled py-2 text-white hover:bg-green-600" onClick={handleSignIn}>Sign in</button>
                 </div>
                 <AnimatePresence>
 
