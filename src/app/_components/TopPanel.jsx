5fac463b2d3bfe8b5531d2ca8539e5673c41fae4
accelor-microsystems <rnd@accelorindia.com>
2024-07-03 16:25:21 +0530

    Update TopPanel.jsx

--- src/app/_components/TopPanel.jsx
+++ src/app/_components/TopPanel.jsx
@@ -6,7 +6,7 @@ import Link from "next/link";
 export default function TopPanel() {
     return (
         <div className="flex w-full items-center justify-between py-6 bg-gray-100">
-            <Link href='/view-data'><button className=" bg-slate-700 text-white px-5 py-3 rounded-lg mx-4 hover:bg-slate-600">View Data</button></Link>
+            <Link target="_blank" href='/view-data'><button className=" bg-slate-700 text-white px-5 py-3 rounded-lg mx-4 hover:bg-slate-600">View Data</button></Link>
             <LogoutButton />
         </div>
     )
