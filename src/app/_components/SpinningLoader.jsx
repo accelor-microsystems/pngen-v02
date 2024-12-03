22e7e5e1ad1fb625e6f7033cb82a77fecd7a07b6
accelor-microsystems <developer@accelorindia.com>
2024-12-03 17:19:08 +0530

    Update SpinningLoader.jsx

--- src/app/_components/SpinningLoader.jsx
+++ src/app/_components/SpinningLoader.jsx
@@ -1,9 +1,10 @@
 export default function SpinningLoader({ cover }) {
     const coverStyle = 'bg-slate-800 h-full w-full flex items-center justify-center bg-opacity-40';
     return (
-        <div className={`loader z-[999] ${cover === true ? coverStyle : ''}`}>
+        <div className={`loader z-[9999] ${cover === true ? coverStyle : ''}`}>
 
-            <div className="lds-ring"><div></div><div></div><div></div><div></div></div>
+            {/* <div className="lds-ring"><div></div><div></div><div></div><div></div></div> */}
+            <div class="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
         </div>
     )
 }
