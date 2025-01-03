cc3c758ca0f08f7c321b8a7f8b196d368bbc4188
accelor-microsystems <developer@accelorindia.com>
2025-01-03 12:40:50 +0530

    Update route.js

--- src/app/api/userExist/route.js
+++ src/app/api/userExist/route.js
@@ -8,12 +8,17 @@ import { redirect } from "next/navigation";
 export async function GET(req) {
     connectMongoDB();
     const { searchParams } = new URL(req.url)
-    const username = searchParams.get('username')
-    const password = searchParams.get('password')
+    const username = searchParams.get('username').replace(/\s+/g, '')
+    const password = searchParams.get('password').replace(/\s+/g, '')
+    console.log(username, password)
 
     const user = await User.findOne({ username: username })
+    console.log(user)
+
+
     if (user) {
         const match = await bcrypt.compare(password, user.password);
+        console.log(match)
         if (match) {
             cookies().set('name', user.name, { secure: true })
             cookies().set('broadCategory', user.department, { secure: true })
