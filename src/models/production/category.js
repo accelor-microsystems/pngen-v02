5f2fc1afaf8227a36996ca50a755a2c8ac52b1eb
accelor-microsystems <developer@accelorindia.com>
2025-03-31 14:39:23 +0530

    Update category.js

--- src/models/production/category.js
+++ src/models/production/category.js
@@ -2,13 +2,14 @@ import mongoose, { Schema } from "mongoose";
 
 const CategorySchema = new Schema(
     {
+        project: { type: String, required: true }, // Reference to the project name
         categoryNumber: Number,
         category: String,
     },
     {
         timestamps: true,
     }
-)
+);
 
-const ProductionCategory = mongoose.models.ProductionCategory || mongoose.model("ProductionCategory", CategorySchema)
+const ProductionCategory = mongoose.models.ProductionCategory || mongoose.model("ProductionCategory", CategorySchema);
 export default ProductionCategory;
